9320af84f69fe427175c3719f77baf0667ab4f5a
King Sam Animations <224757196+kingsamanimations@users.noreply.github.com>
2026-06-08 11:33:20 +0100

    Add functions for building, inserting, and deleting data in root

--- binary.js
+++ binary.js
@@ -16,32 +16,6 @@ class node {
         this.left = left;
         this.right = right;
     }
-
-    buildTree(array) {
-        return inputArray(array, 0, array.length -1);
-    }
-
-    includes(value) {}
-
-    insert(value) {}
-
-    deleteItem(value) {}
-
-    levelOrderForEach(callback) {}
-
-    inOrderForEach(callback) {}
-
-    preOrderForEach(callback) {}
-
-    postOrderForEach(callback) {}
-    
-    height(value) {}
-
-    depth(value) {}
-
-    isBalanced() {}
-
-    rebalance() {}
 }
 
 class Tree {
@@ -66,7 +40,76 @@ class Tree {
 
         return root;
     }
+
+    buildTree(array) {
+        return inputArray(array, 0, array.length -1);
+    }
+
+    includes(value) {}
+
+    insert(value, root = this.root) {
+        if (root == null) {
+            return (root = new node(value));
+        }
+
+        if (root.data < value) {
+            root.right = (this.insert(value, root.right));
+        } else {
+            root.left = (this.insert(value, root.left));
+        }
+        prettyPrint(this.root);
+        return root;
+    }
+
+    deleteItem(value, root = this.root) {
+        if (root == null) {
+            return root;
+        }
+
+        if (root.data > value) {
+            root.left = (this.insert(value, root.left));
+        } else if (root.data < value) {
+            root.right = (this.insert(value, root.right));
+        } else {
+            if(root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+            root.data = minValue(root);
+            root.right = this.deleteItem(root.right, root.data);
+        }
+        prettyPrint(this.root);
+        return root;
+    }
+
+    levelOrderForEach(callback) {}
+
+    inOrderForEach(callback) {}
+
+    preOrderForEach(callback) {}
+
+    postOrderForEach(callback) {}
+    
+    height(value) {}
+
+    depth(value) {}
+
+    isBalanced() {}
+
+    rebalance() {}
 }
+
+// Function to make root min and data equal data and left, as long as root isn't null
+function minValue(root) {
+    let min = root.data;
+    while (root != null) {
+        min = root.data;
+        root = root.left;
+    }
+    prettyPrint(this.root);
+    return min;
+}; 
 
 let arrayTest = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 balanced = new Tree (arrayTest, 1, 7);
