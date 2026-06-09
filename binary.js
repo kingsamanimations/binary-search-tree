4c78f4a494ba7cd08ea7613c285b25221f7d4350
King Sam Animations <224757196+kingsamanimations@users.noreply.github.com>
2026-06-09 12:36:53 +0100

    Add height, depth, and balance functions within class tree

--- binary.js
+++ binary.js
@@ -26,6 +26,7 @@ class Tree {
             inputArray.length - 1
         );
         prettyPrint(this.root);
+        this.inputArray = [...removeDuplicates(mergeSort(inputArray))];
         this.preOrderData = [];
         this.inOrderData = [];
         this.postOrderData = [];
@@ -115,7 +116,7 @@ class Tree {
         if (root.data !== undefined) {
             this.inOrderData.push(root.data);
         }
-        console.log("Print tree in order: " `${this.inOrderData}`);
+        console.log("Print tree in order: ", `${this.inOrderData}`);
     }
 
     preOrderForEach(root = this.root) {
@@ -132,11 +133,11 @@ class Tree {
         if (root.data !== undefined) {
             this.preOrderData.push(root.data);
         }
-        console.log("Print tree pre-orderd: " `${this.preOrderData}`);
+        console.log("Print tree pre-orderd: ", `${this.preOrderData}`);
     
     }
 
-    postOrderForEach(callback) {
+    postOrderForEach(root = this.root) {
         if (root == null) return;
 
         if (root.left !== null) {
@@ -150,13 +151,13 @@ class Tree {
         if (root.data !== undefined) {
             this.postOrderData.push(root.data);
         }
-        console.log("Print tree post-ordered: " `${this.postOrderData}`);
+        console.log("Print tree post-ordered: ", `${this.postOrderData}`);
     
     }
     
     height(root = this.root) {
         if (root == null) {
-            return 1;
+            return -1;
         } else {
             let left = this.height(root.left);
             let right = this.height(root.right);
@@ -165,11 +166,54 @@ class Tree {
         }
     }
 
-    depth(value) {}
+    depth(nodeVal, root = this.root, edgeCount = 0) {
+        if (root ===  null) return;
+        if (root.data === nodeVal) return edgeCount;
 
-    isBalanced() {}
+        if (root.data < nodeVal) {
+            return this.depth(nodeVal, root.right, (edgeCount + 1));
+        } else {
+            return this.depth(nodeVal, root.left, (edgeCount + 1));
+        }
+    }
 
-    rebalance() {}
+    isBalanced(root = this.root) {
+        if (root === null) return undefined;
+
+        let lefthalf = root.left;
+        let righthalf = root.right;
+
+        if (Math.abs(this.height(lefthalf) - this.height(righthalf) > 1)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    rebalance() {
+        prettyPrint(this.root);
+        if(this.isBalanced(this.root)) return this.root;
+
+        let balancedArray = [];
+        balancedArray = this.traverse(this.root, balancedArray);
+
+        let balancedTree = new Tree(balancedArray);
+        prettyPrint(balancedTree.root);
+        console.log("Is tree balanced? ", balancedTree.isBalanced());
+        return balancedTree.root;
+    }
+
+    // Traversal for rebalance. To provide a new array
+    traverse(root, array) {
+        if (array != undefined) array.push(root.data);
+        if (root.left !== null) {
+            this.traverse(root.left, array);
+        }
+        if (root.right !== null) {
+            this.traverse(root.right, array);
+        }
+        return array;
+    }
 }
 
 // Function to make root min and data equal data and left, as long as root isn't null
@@ -181,7 +225,43 @@ function minValue(root) {
     }
     prettyPrint(this.root);
     return min;
-}; 
+};
 
+// Functions for removing duplicates
+function removeDuplicates(inputArray) {
+    return [...new Set(inputArray)];
+}
+
+function mergeSort(inputArray) {
+    if (inputArray.length == 1) return inputArray;
+
+    const newArray = [];
+
+    const left = mergeSort(inputArray.slice(0, inputArray.length / 2));
+    const right = mergeSort(inputArray.slice(inputArray.length / 2));
+
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            newArray.push(left.shift());
+        } else {
+            newArray.push(right.shift());
+        }
+
+        return [...newArray, ...left, ...right];
+    }
+}
+// functions end here
+
+// Tie it all together
 let arrayTest = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 balanced = new Tree (arrayTest, 1, 7);
+balanced.insert(56);
+balanced.insert(78);
+balanced.insert(100);
+balanced.preOrderForEach();
+balanced.inOrderForEach();
+balanced.postOrderForEach();
+console.log("Height: ", balanced.height()); //Logs 5
+console.log("depth: ", balanced.depth(7)); // Logs 3
+console.log("Tree balanced? ", balanced.isBalanced());
+console.log("Re-balance tree: ", balanced.rebalance());
